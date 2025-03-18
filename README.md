bb745800440069223fc3044e46706a15c43d7185
pulkitg-flwls <pulkit.gera@flawlessai.com>
2025-03-18 11:07:40 +0000

    added film grain codes

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Grain-Degrain Project
 
 Code consisting of Grain/Degrain pipeline and analysis
+
+## Add Film Grain
+`python video_grain --input_dir <dir_with_video> --output_dir <output_dir> --easy_int 0.1 --hard_int 0.4 --blend_mode ADD --preset KODAK_VISION3`
--- utils/video_grain.py
+++ utils/video_grain.py
@@ -10,10 +10,16 @@ def parse_args():
     parser = ArgumentParser(description="Load image dataset")
     parser.add_argument("--input_dir", type=str, default="")
     parser.add_argument("--output_dir", type=str, default="")
+    parser.add_argument("--easy_int", type=float, default=0.1)
+    parser.add_argument("--hard_int", type=float, default=0.4)
+    parser.add_argument("--preset", choices=[p.name for p in GrainPreset], 
+                       default="KODAK_5248", help="Film stock preset")
+    parser.add_argument("--blend", choices=["ADD", "OVERLAY", "SOFT_LIGHT"], 
+                       default="OVERLAY", help="Grain blend mode")
     args = parser.parse_args()
     return args
 
-def process_video(input_path, output_path, preset=GrainPreset.KODAK_5248, intensity_scale=1.0):
+def process_video(input_path, output_path, preset=GrainPreset.KODAK_VISION3, intensity_scale=1.0,blend_mode=BlendMode.ADD):
     """
     Reads a video file, applies film grain to each frame, and saves the output.
     
@@ -47,7 +53,7 @@ def process_video(input_path, output_path, preset=GrainPreset.KODAK_5248, intens
                 frame = frame.astype(np.float32) / 255.0
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 
-                grained_frame = apply_film_grain(frame, preset=preset, intensity_scale=intensity_scale, blend_mode=BlendMode.ADD)
+                grained_frame = apply_film_grain(frame, preset=preset, intensity_scale=intensity_scale, blend_mode=blend_mode)
                 
                 grained_frame = cv2.cvtColor(grained_frame, cv2.COLOR_RGB2BGR)
                 grained_frame = (grained_frame * 255).astype(np.uint8)
@@ -73,7 +79,8 @@ if __name__ == "__main__":
     os.makedirs(args.output_dir, exist_ok=True)
     os.makedirs(os.path.join(args.output_dir, 'easy'), exist_ok=True)
     os.makedirs(os.path.join(args.output_dir, 'hard'), exist_ok=True)
-
+    preset = GrainPreset[args.preset]
+    blend_mode = BlendMode[args.blend]
     files = [p for p in os.listdir(args.input_dir) if p.endswith('.mp4')]
     
     for video in files[1:]:
@@ -82,5 +89,5 @@ if __name__ == "__main__":
         hard_path = os.path.join(args.output_dir, 'hard', video)
 
         # Process videos one by one to avoid OOM
-        process_video(input_path, hard_path, preset=GrainPreset.KODAK_VISION3, intensity_scale=0.4)
-        process_video(input_path, easy_path, preset=GrainPreset.KODAK_VISION3, intensity_scale=0.1)
+        process_video(input_path, hard_path, preset=preset, intensity_scale=args.hard_int, blend_mode=blend_mode)
+        process_video(input_path, easy_path, preset=preset, intensity_scale=args.easy_int, blend_mode=blend_mode)
